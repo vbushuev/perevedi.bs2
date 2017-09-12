78dd92649bbf8e40b9c5dbaefc868bd251661796
vbushuev <v.bushuev@gmail.com>
2017-09-12 11:52:32 +0300

    worked endpoints

--- config.php
+++ config.php
@@ -11,9 +11,9 @@ $ariuspay = [
     "prod" => [// testdata
         "TransferRequest" => [
             "url" => "https://gate.payneteasy.com/paynet/api/v2/",
-            "endpoint" => "2593",
-            "merchant_key" => "39DF3531-5881-4799-BA24-4415047AB4C4",
-            "merchant_login" => "rentkomplekt_test"
+            "endpoint" => "5818",
+            "merchant_key" => "E51B378B-0018-4A7A-B327-C758CC219FE3",
+            "merchant_login" => "rentkomplekt"
         ]
     ],
 ];
